f44b640ff1691e78015c162baf9a33ba7434ed7d
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2022-09-08 21:14:34 +0400

    ref sqldatareader

---
+++ Demo.ConsoleTest/DataReaderHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.ConsoleTest
+{
+	internal static class DataReaderHelper
+	{
+		internal static T? GetFieldValue<T>(this SqlDataReader reader, string name)
+		{
+			T? ret = default;
+			if (!reader[name].Equals(DBNull.Value))
+			{
+				ret = (T)reader[name];
+			}
+
+			return ret;
+		}
+	}
+}
--- Demo.ConsoleTest/Program.cs
+++ Demo.ConsoleTest/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 using System.Text;
+using Demo.ConsoleTest;
 
 namespace demo.ConsoleTest
 {
@@ -24,6 +26,8 @@ namespace demo.ConsoleTest
 				connectionInformation = ex.ToString();
 			}
 
+			GetLoanData(1);
+
 			Console.WriteLine(connectionInformation);
 
 			Console.ReadKey();
@@ -41,6 +45,39 @@ namespace demo.ConsoleTest
 			}
 
 			return amount;
+		}
+
+		private static void GetLoanData(int id)
+		{
+			using (var connection = new SqlConnection("server=localhost;database=LMS_Trunk;trusted_connection=true;"))
+			{
+				using (var command = connection.CreateCommand())
+				{
+					command.CommandText = "select * from loan.LOANS where LOAN_ID = @id; select * from loan.LOANS where LOAN_ID = @id + 1";
+					command.Parameters.Add(new SqlParameter("@id", id));
+
+					connection.Open();
+					using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+					{
+						while (reader.Read())
+						{
+							Console.WriteLine(reader["LOAN_ID"]);
+							Console.WriteLine(reader.GetDecimal(reader.GetOrdinal("AMOUNT")));
+							Console.WriteLine(reader.GetFieldValue<decimal>(reader.GetOrdinal("USED_AMOUNT")));
+							Console.WriteLine(reader.GetFieldValue<decimal?>("FEE1"));
+						}
+
+						reader.NextResult();
+						// next result set; i.e. multiple select statements;
+						while (reader.Read())
+						{
+							Console.WriteLine(reader.GetFieldValue<int>(reader.GetOrdinal("LOAN_ID")));
+							Console.WriteLine(reader.GetDecimal(reader.GetOrdinal("AMOUNT")));
+							Console.WriteLine(reader.GetFieldValue<decimal?>("USED_AMOUNT"));
+						}
+					}
+				}
+			}
 		}
 
 		private static string GetConnectionInformation(SqlConnection connection)
